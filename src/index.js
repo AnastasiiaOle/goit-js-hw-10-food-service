abc8e99922b2b897f67428cfc7c78fb505bea625
AnastasiiaOle <70802218+AnastasiiaOle@users.noreply.github.com>
2021-02-25 22:27:26 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './styles.css';
+import cardsTemplate from './templates/cards.hbs';
+
+import cards from './menu.json';
+
+const markup = cardsTemplate(cards);
+const galleryRef = document.querySelector('.js-menu');
+galleryRef.insertAdjacentHTML('afterbegin', markup);
